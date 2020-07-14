4fee07d021e02bca8db392a1f432c18d85a6302b
Alex <alex.udarbe@gmail.com>
2020-07-14 11:52:58 -0700

    Mongoose, dotenv, atlas connect

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+// Connect to DB with Mongoose
+require('dotenv').config()
+require('./config/database')
+
 const app = express();
 
 app.use(logger('dev'));
@@ -12,13 +16,13 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Catch All
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Server on port 3001  
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-console.log(`Express app running on port ${port}`)
-});  
+app.listen(port, function () {
+	console.log(`Express app running on port ${port}`)
+});
