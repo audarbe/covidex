ab2e685a3597658ba2c09d3c80c557490d79699d
Alex <alex.udarbe@gmail.com>
2020-07-18 11:57:22 -0700

    Dynamic map color pt1

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import USAMap from "react-usa-map";
 import './Map.css'
-import { Redirect } from 'react-router-dom';
 
 class Map extends Component {
     statesCustomConfig = () => {
         let stateFill = this.props.stateStatistics.reduce((acc, s) => {
-            let stateName = s.state
-            let fillColor = {fill: 'blue'}
-            return {...acc, [stateName]: fillColor}
+            let fillColor = {fill: this.getFillColor(s.positiveIncrease)}
+            return {...acc, [s.state]: fillColor}
         },{})
         return stateFill;
     };
 
-// helper function with switch statement for color fills
+    getFillColor = (val) => {
+        switch (true) {
+        case val > 10000:
+            console.log('true')
+            return '#C0392B';
+        case val > 7500:
+            return '#D35400';
+        case val > 5000:
+            return '#E67E22';
+        case val > 2500:
+            return '#F39C12';
+        case val > 1000:
+            return '#F1C40F';
+        case val === 0:
+            return '#A9DFBF';
+        default:
+            return '#D7DBDD';
+        }
+    }
 
     render() {
         return (
