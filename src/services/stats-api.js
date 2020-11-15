de10634239bb59b43c373e0fc525d42974bfeecf
Alex <alex.udarbe@gmail.com>
2020-11-15 12:36:19 -0800

    Update API Endpoints

--- src/services/stats-api.js
+++ src/services/stats-api.js
@@ -1,17 +1,17 @@
 export function getStatistics(currentChoice) {
     if (currentChoice === 'US') {
         return (
-            fetch(`https://covidtracking.com/api/v1/us/current.json`, { mode: 'cors' }).then(res => res.json())
+            fetch(`https://api.covidtracking.com/v1/us/current.csv`, { mode: 'cors' }).then(res => res.json())
         );
     } else {
         return (
-            fetch(`https://covidtracking.com/api/v1/states/${currentChoice.toLowerCase()}/current.json`, { mode: 'cors' }).then(res => res.json())
+            fetch(`https://api.covidtracking.com/v1/states/${currentChoice.toLowerCase()}/current.json`, { mode: 'cors' }).then(res => res.json())
         );
     }
 }
 
 export function getStateStatistics() {
     return (
-        fetch(`https://covidtracking.com/api/v1/states/current.json`, { mode: 'cors' }).then(res => res.json())
+        fetch(`https://api.covidtracking.com/v1/states/current.csv`, { mode: 'cors' }).then(res => res.json())
     );
 }
